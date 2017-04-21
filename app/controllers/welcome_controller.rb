58ce75c0188471bc85d2c941b0b7ea9012d862f2
Wyatt <thewyatt@gmail.com>
2017-04-21 13:35:26 -0700

    Adds react class for Welcome. Displays welcome index as root.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+
+  #public welcome page
+  def index
+    @hello_world_props = { name: "Stranger" }
+  end
+
+  #send to profile page
+  def show
+
+  end
+
 end
