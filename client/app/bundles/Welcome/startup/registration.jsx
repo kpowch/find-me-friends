8b70a2ec0fdb84b1e5e4f80d5868fa52ab839778
Wyatt <thewyatt@gmail.com>
2017-04-21 14:09:56 -0700

    Updates untracked files from last feature commit. *slap on wrist

---
+++ client/app/bundles/Welcome/startup/registration.jsx
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Welcome from '../components/Welcome';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  Welcome,
+});
