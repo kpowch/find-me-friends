025a0b734795832c243645160e9662a886245b94
Wyatt <thewyatt@gmail.com>
2017-04-21 16:59:56 -0700

    Adds user object creation in profile controller. Adds user_params method and defines permitted inputs.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,35 @@
 class ProfileController < ApplicationController
+
   #show user profile
   def index
     @user = User.where(session[:user_id] = params[:id])
   end
+
   # update user stats
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:profile], notice: 'Profile updated, friend!'
+    else
+      render :new
+    end
   end
+
   #remove profile from databse, return to welcome?
   def destroy
   end
 
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password_digest,
+      :dob,
+      :profile_picture,
+      :bio
+    )
+  end
 end
