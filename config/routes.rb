cdde6d315d609a00676108f24585b0b2126645db
Wyatt <thewyatt@gmail.com>
2017-04-21 12:07:10 -0700

    Adds routes for welcome page index and show.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #welcome page
+  root 'welcome#index'
+  resources :welcome, only: [:index, :show]
+
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
