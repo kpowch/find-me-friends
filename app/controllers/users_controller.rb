5fd567aded8b856a443bdf1ab6c7041d6b648c45
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-22 14:14:50 -0700

    refactors links to use new_user_path instead of users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 
+  # For a new user to signup
   def new
   end
 
+  # To create the user that just signed up
   def create
     @user = User.new(user_params)
     p @user.inspect
@@ -12,11 +14,23 @@ class UsersController < ApplicationController
       redirect_to profiles_path(params[:product_id])
     else
       flash[:alert] = @user.errors.full_messages.to_s
-      redirect_to 'users/new'
+      redirect_to new_user_path
     end
   end
 
+  # To show the user's settings
+  def show
+  end
 
+  # To edit settings (HTML form)
+  def edit
+  end
+
+  # Updates edited settings
+  def update
+  end
+
+  # TODO delete a user?
 
   private
   def user_params
