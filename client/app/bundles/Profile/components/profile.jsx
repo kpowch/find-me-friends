a940c96aee460ce9296b4fed0ad16ed39f52c58d
Wyatt <thewyatt@gmail.com>
2017-04-22 12:40:50 -0700

    Updates commenting and removes unnecessary example code from profiles.jsx

--- client/app/bundles/Profile/components/profile.jsx
+++ client/app/bundles/Profile/components/profile.jsx
@@ -15,34 +15,10 @@ export default class Welcome extends React.Component {
   constructor(props, _railsContext) {
     super(props);
 
-    // How to set initial state in ES6 class syntax
-    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
+    // this.state = { name: this.props.name }; FROM HELLO WORLD EXAMPLE
   }
 
-  updateName = (name) => {
-    this.setState({ name });
-  };
-
   render() {
-    return (
-      <div>
-        <h3>
-          Holla, {this.state.name}!
-        </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
-      </div>
-    );
+    return ()
   }
 }
