750be57b027ffb147b4eb898b567ea07b99bd1f4
Wyatt <thewyatt@gmail.com>
2017-04-22 15:03:47 -0700

    Adds react components Sidebar and SuggestionsList to Profile.jsx. Moves registration folder to client/app. Updates registration to include both Welcome and Profile.

---
+++ client/app/bundles/Profile/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Sidebar extends React.Component {
+
+  render() {
+    return (
+      <main className="sidebar">
+        <div className="sidebar-name"></div>
+        <div className="sidebar-picture"></div>
+        <div className="sidebar-name"></div>
+        <div className="sidebar-name"></div>
+      </main>
+    );
+  }
+}
+
+export default Sidebar;
--- client/app/bundles/Profile/components/profile.jsx
+++ client/app/bundles/Profile/components/profile.jsx
@@ -1,9 +1,9 @@
-// TODO THIS FILE IS NOT BEING USED
+
 
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class Welcome extends React.Component {
+export default class Profile extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
@@ -12,13 +12,38 @@ export default class Welcome extends React.Component {
    * @param props - Comes from your rails view.
    * @param _railsContext - Comes from React on Rails
    */
+
   constructor(props, _railsContext) {
     super(props);
 
-    // this.state = { name: this.props.name }; FROM HELLO WORLD EXAMPLE
+    this.state = {
+      currentUser: userStats(this.props.user),
+      friendsList: []
+     };
+  }
+
+  /* Need method to add friend panels */
+  addFriend(int) {
+    for (let i = int; i>0; i++){
+      nextFriend =
+      this.state.friendsList.push()
+    }
+  }
+
+  userStats(user) {
+
   }
 
   render() {
-    return ()
+    return (
+      <div className="profiles">
+        <div>
+          <SuggestionList friendsList={this.state.friendsList} addFriend={this.state.addFriend}>
+        </div>
+        <div className="sidebar">
+          <Sidebar >
+        </div>
+      </div>
+      )
   }
 }
