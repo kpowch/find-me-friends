804346408f80f93354c8e21ee33637ae0b65c670
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-22 23:20:23 -0700

    fixes scope authentical with credentials function

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,18 +14,18 @@ class User < ApplicationRecord
   has_many :messages
   has_many :chatrooms, through: :messages
 
+  validates :first_name, :last_name, :email, presence: true
   validates_uniqueness_of :email, case_sensitive: false
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :password, presence: true
-  # validates :password, length: length: { in: 4..20 }
+  validates :password, :password_confirmation, presence: true
+  validates :password, length: { in: 4..15 }
 
-  def authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip)
 
     if user && user.authenticate(password)
-      session[:user_id] = user.id
-      user
+      return user
+    else
+      return nil
     end
   end
 end
