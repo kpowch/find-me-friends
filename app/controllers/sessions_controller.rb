1798bfc44a5dea58ddfc1571b8ff98fa3ebd3d64
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-22 23:21:54 -0700

    adds functionality to sign in and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
+  # renders login form
   def new
   end
 
+  # to receive form and sign in user
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
-      redirect_to '/'
+    # authenticate email-password combo and strip email of edge whitespace
+    if user = User.authenticate_with_credentials(params[:email].strip, params[:password])
+      session[:user_id] = user.id
+      redirect_to '/' # TODO change this redirect to profile
     else
-      flash[:alert] = "There was an error with your credentials. Please Try again."
+      flash[:alert] = "There was an error with your credentials. Please try again."
       redirect_to '/login'
     end
   end
 
+  # to logout
   def destroy
+    flash[:alert] = "You have successfully been logged out. Come back soon!"
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/login'
   end
-
-
 end
