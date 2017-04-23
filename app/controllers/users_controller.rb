2aa975f4a04d3e8dd1df1837723b8fc3321b5639
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-22 22:46:37 -0700

    implements signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,39 @@
 class UsersController < ApplicationController
-
-  # For a new user to signup
+  # to render the signup form
   def new
   end
 
-  # To create the user that just signed up
+  # to receive the form and create a user with form params
   def create
     @user = User.new(user_params)
-    p @user.inspect
 
     if @user.save
-      session[:user_id] = user.id
-      redirect_to profiles_path(params[:product_id])
+      session[:user_id] = @user.id
+      # redirect to edit user path so they can input their interests (not profiles_path yet)
+      redirect_to '/' # edit_user_path(:user_id)
     else
-      flash[:alert] = @user.errors.full_messages.to_s
+      flash[:alert] = @user.errors.full_messages.to_s # TODO incorporate this into page
       redirect_to new_user_path
     end
   end
 
-  # To show the user's settings
+  # render user's settings
+  # TODO this might have to connect to the profile sidebar
   def show
   end
 
-  # To edit settings (HTML form)
+  # to receive form and edit settings (HTML form)
   def edit
   end
 
-  # Updates edited settings
+  # updates edited settings
   def update
   end
 
   # TODO delete a user?
 
   private
+
   def user_params
     params.require(:user).permit(
       :first_name,
