77301f09b5c1494f1b1c0063637cdc0a19bba3f2
Paige <paigekato@gmail.com>
2017-04-24 12:41:57 -0700

    Adds files for Friendships

---
+++ client/app/bundles/Chatrooms/components/Friendships.jsx
@@ -0,0 +1,10 @@
+import React from'react';
+
+class Friendships extends React.Component {
+  render() {
+    return (
+
+    );
+  }
+}
+export default Friendships;
--- client/app/bundles/Chatrooms/components/chatrooms.jsx
+++ client/app/bundles/Chatrooms/components/chatrooms.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 
 import Chatbar from './Chatbar.jsx';
 import Messages from './Messages.jsx';
+import Friendships from './Friendships.jsx';
 
 export default class Chatrooms extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
 
-  /**
-   * @param props - Comes from your rails view.
-   * @param _railsContext - Comes from React on Rails
-   */
+
   constructor(props, _railsContext) {
     super(props);
 
@@ -21,13 +19,13 @@ export default class Chatrooms extends React.Component {
       messages: this.props.messages,
       friendships: this.props.friendships
     };
-    // this.state = { name: this.props.name }; FROM HELLO WORLD EXAMPLE
+
   }
 
   render() {
     return (
     <div className='chatroom'>
-      {this.state.name}
+
       <div className='sidebar'>
         <Messages />
 
