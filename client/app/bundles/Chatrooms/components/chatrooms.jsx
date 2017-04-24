1ecacdc1196fac361e52f2386920d340e0078e64
Paige <paigekato@gmail.com>
2017-04-24 12:34:17 -0700

    Adds changes to webpack, controllers and client

--- client/app/bundles/Chatrooms/components/chatrooms.jsx
+++ client/app/bundles/Chatrooms/components/chatrooms.jsx
@@ -16,19 +16,26 @@ export default class Chatrooms extends React.Component {
   constructor(props, _railsContext) {
     super(props);
 
-    this.state = { name: this.props.name };
+    this.state = {
+      name: this.props.name,
+      messages: this.props.messages,
+      friendships: this.props.friendships
+    };
     // this.state = { name: this.props.name }; FROM HELLO WORLD EXAMPLE
   }
 
   render() {
     return (
-     <div>
-        <h3>
-          Holla, {this.state.name}!
-          // <Messages />
-          // <Chatbar />
-        </h3>
+    <div className='chatroom'>
+      {this.state.name}
+      <div className='sidebar'>
+        <Messages />
+
       </div>
+      <div>
+        <Chatbar currentUser={this.state.name} />
+      </div>
+    </div>
 
     )
   }
