eecdb7acd947f4d3c794332dcef98834bcfde11f
Paige <paigekato@gmail.com>
2017-04-25 17:08:12 -0700

    Changes value for message box

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -6,7 +6,7 @@ function submitNewMessage(){
   $('textarea#message_content_test').keydown(function(event) {
     if (event.keyCode == 13) {
         $('[data-send="message"]').click();
-        $('[data-textarea="message"]').val(" ")
+        $('[data-textarea="message"]').val("")
         return false;
      }
   });
