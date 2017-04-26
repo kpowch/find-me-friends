9f4e916617d8fb2f5b7686b039c5a17f49b95603
Paige <paigekato@gmail.com>
2017-04-25 21:59:40 -0700

    Cleans up code

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -5,11 +5,8 @@ App.messages = App.cable.subscriptions.create('MessagesChannel', {
   },
 
   renderMessage: function(data) {
-    console.log(data, "EFEW")
-    // if (data.user === data.current_user) {
-     // return "<div> <p class='currentUser'> <b>" + data.user + ": </b>" + data.message + "</p> </div>";
-    // } else {
+
       return "<div> <p> <b>" + data.user + ": </b>" + data.message + "</p> </div>";
-    // }
+
   }
 });
