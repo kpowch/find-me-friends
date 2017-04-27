fbfb51008ba7d8132b53b6d71f02d21448a84499
Paige <paigekato@gmail.com>
2017-04-26 21:56:24 -0700

    Removed line from config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,7 +12,7 @@ const config = {
     'es5-shim/es5-shim',
     'es5-shim/es5-sham',
     'babel-polyfill',
-    './app/startup/registration.jsx',
+    './app/startup/registration.jsx'
   ],
 
   output: {
