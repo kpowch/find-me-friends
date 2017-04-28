4e471eba3135f79ffb00108aedf4f3b2bf677c60
Paige <paigekato@gmail.com>
2017-04-27 21:15:18 -0700

    Adds index page for Admin/Chatroom

--- app/controllers/admin/chatrooms_controller.rb
+++ app/controllers/admin/chatrooms_controller.rb
@@ -5,8 +5,34 @@ class Admin::ChatroomsController < ApplicationController
   end
 
   def index
+    @chatrooms = Chatroom.all
+    @userChatrooms = Chatroom.joins(:users).where(friendships: { user: 2 })
+    @friendChatrooms = Chatroom.joins(:users).where(friendships: { friend: 2 })
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+
+    if @chatroom.save
+      redirect_to '/admin'
+    else
+      redirect_to '/admin'
+    end
   end
 
   def destroy
+    @chatroom = Chatroom.find params[:id]
+    @chatroom.destroy
+    redirect_to '/admin/chatrooms', notice: "Chatroom deleted successfully."
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:chatroom_id)
   end
 end
