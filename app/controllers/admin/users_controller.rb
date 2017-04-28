358025abc3a2f21ffe57a09507df5832c9523bcc
Paige <paigekato@gmail.com>
2017-04-27 21:25:49 -0700

    Updates Admin Users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,43 @@
 class Admin::UsersController < ApplicationController
   http_basic_authenticate_with name: 'tellmewhatyouwant', password: 'whatyoureallyreallywant'
 
-  def show
-  end
-
   def new
   end
 
+  def show
+    @user = User.order(id: :desc).all
+  end
+
   def update
+    @user = User.find params[:id]
+
+    if @user[:interest_ids].nil?
+      @user.update(interest_ids: [])
+    end
+    @user.update(@user)
+    redirect_to 'admin/users'
   end
 
   def destroy
+    @user = User.find params[:id]
+    @user.destroy
+    redirect_to 'admin/users', notice: 'User deleted!'
+  end
+
+  private
+  def user_params
+    params.require(:user)
+    .permit(
+      :first_name,
+      :last_name,
+      :dob,
+      :email,
+      :profile_picture,
+      :password,
+      :password_confirmation,
+      :bio,
+      interest_ids: []
+    )
   end
 
 end
