ad181de1136185667fc8af35d278b11536a89d0a
Wyatt <thewyatt@gmail.com>
2017-04-27 17:32:31 -0700

    Adds react friend-cards on profile page. Does not limit number of friendships displayed, yet.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,11 +16,21 @@ class ProfilesController < ApplicationController
   end
 
   def friendly_three_amigos_method
-    full_user_objects = {}
+    full_user_objects = []
     friendships = Friendship.where(user_id: current_user.id)
     friendships.each do |friendship|
-      full_user_objects[friendship.id] = User.where(id: friendship.friend_id)
+      current = User.where(id: friendship.friend_id)
+      current_hash = current.as_json
+      current_person = current_hash[0]
+      full_user_objects.push({
+        first_name: current_person["first_name"],
+        last_name: current_person["last_name"],
+        email: current_person["email"],
+        profile_picture: current_person["profile_picture"],
+        bio: current_person["bio"]
+        })
     end
+    puts full_user_objects
     full_user_objects
   end
 end
