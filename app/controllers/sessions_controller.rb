ba20a31e784ef365db6b453fc09af12b8db453e8
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-29 13:31:49 -0700

    Fixes incorrect redirections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,25 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # to receive form and sign in user
+  # receive form and sign in user
   def create
     # authenticate email-password combo and strip email of edge whitespace
     if user = User.authenticate_with_credentials(params[:email].strip, params[:password])
       session[:user_id] = user.id
-      ## should be saving friendships everytime a user logs in
+      # should be saving friendships everytime a user logs in
+      # TODO do we need this here?
       save_friendships
-      redirect_to profiles_path(current_user.id)
+      redirect_to profiles_path
     else
       flash[:alert] = "There was an error with your credentials. Please try again."
       redirect_to '/login'
     end
   end
-  #
-  # to logout
+
+  # logout
   def destroy
     flash[:alert] = "You have successfully been logged out. Come back soon!"
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to :root
   end
 end
