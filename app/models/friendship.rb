2395f3b21fc022529eaf6df8f64a28bddb4ae75e
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-29 15:34:40 -0700

    Adds validations to models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,12 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  validates :user, presence: true
+  validates :friend, presence: true
+  validates :friendship_status, presence: true
+  validates :friendship_status, inclusion: {
+    in: %w(pending accepted declined),
+    message: "%{value} is not a valid status"
+  }
 end
--- app/models/interests_user.rb
+++ app/models/interests_user.rb
@@ -1,6 +1,4 @@
 class InterestsUser < ApplicationRecord
-  # has_many :interests_users
-  # has_many :users, through: :interests_users
   belongs_to :user
   belongs_to :interest
 end
