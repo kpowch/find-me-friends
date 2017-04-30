abce9b4a5414b4fc8199a983d3dd012d54357939
Paige <paigekato@gmail.com>
2017-04-29 19:53:21 -0700

    Adds email for new friendship

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,11 @@ class UserMailer < ApplicationMailer
     mail(to:@user.email, subject: 'Welcome to Find Me Friends')
   end
 
+  def new_friend(user, friend)
+    @user = user
+    @friend = friend
+    mail(to:@user.email, subject: 'You Have a New Friend!')
+  end
+
+end
 end
