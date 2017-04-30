6ce0a172e323614e358949a55c3b6f343a24c0fb
Paige <paigekato@gmail.com>
2017-04-30 15:12:42 -0700

    Removes boilerplate generated RSPEC files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'should create a valid user' do
+      user = User.create(
+        first_name: "Paige",
+        last_name: "Kato",
+        email: "paige@meow.com",
+        password: "1234PaigeIsAwesome",
+        password_confirmation: "1234PaigeIsAwesome",
+        dob: "1997-02-02",
+        bio: "I am the best"
+        )
+      expect(user).to (be_valid)
+    end
+  end
+
+
 end
