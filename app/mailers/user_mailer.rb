d1ff21d9ce9b189a84364ef1c0978f8e1c69e345
Paige <paigekato@gmail.com>
2017-04-29 18:28:21 -0700

    Adds html/text files for emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+
+  def welcome_email(user)
+    @user = user
+    @url = 'findmefriends.com'
+    mail(to:@user.email, subject: 'Welcome to Find Me Friends')
+  end
+
 end
