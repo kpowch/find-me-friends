1ee397d597b9953c5bfdb39539227a76a850b62e
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-30 18:50:51 -0700

    Modifies pending friend method to list friends who initiated a friendship

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -41,24 +41,26 @@ class ProfilesController < ApplicationController
     full_user_objects
   end
 
+  # returns an array of users who have asked the user to be a friend
   def pending_three_amigos_method
-    full_user_objects = []
-    pending = Friendship.where(user_id: current_user.id).where(friendship_status: "pending" && "Pending")
-    pending.each do |friendship|
-      current = User.where(id: friendship.friend_id)
-      current_hash = current.as_json
-      current_person = current_hash[0]
-      full_user_objects.push({
-        id: current_person["id"],
-        friendship_id: friendship.id,
-        first_name: current_person["first_name"],
-        last_name: current_person["last_name"],
-        email: current_person["email"],
-        profile_picture: current_person["profile_picture"],
-        bio: current_person["bio"]
-        })
+    pendingFriends = []
+    # TODO friend_id is currently hard-coded to 2. should be current_user.id
+    pendingFriendships = Friendship.where("friend_id = ? AND friendship_status = ?", 7, "pending")
+    pendingFriendships.each do |friendship|
+      # 'user' is the person who initiated the friendship
+      user = User.find(friendship.user_id)
+      # add the user's info to the array
+      pendingFriends.push({
+        id: user.id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        dob: user.dob,
+        profile_picture: user.profile_picture.thumb.url,
+        bio: user.bio
+      })
     end
-    puts full_user_objects
-    full_user_objects
+    # puts "\n\n\n\n\n\n\n\n\n\n pending friends: #{pendingFriends} \n\n\n\n\n"
+    pendingFriends
   end
 end
