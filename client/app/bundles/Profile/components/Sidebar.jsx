7488509e247d124d48f746c2a7d8f7a5388c67ee
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-05-03 23:50:13 -0700

    Adds functionality to notifications

--- client/app/bundles/Profile/components/Sidebar.jsx
+++ client/app/bundles/Profile/components/Sidebar.jsx
@@ -4,6 +4,23 @@ import Notifications from './Notifications';
 
 export default class Sidebar extends React.Component {
 
+  // delete a notification when read and refresh list
+  deleteNotification = (notification) => {
+    return (ev) => {
+      ev.preventDefault();
+      // send ajax to delete a notification
+      $.ajax({
+        data: {
+          id: notification.id,
+        },
+        url: '/notifications/' + notification.id,
+        type: 'DELETE',
+        dataType: 'json',
+        success: () => window.location = window.location.origin + "/chatrooms/" + notification.chatroom_id
+      });
+    };
+  }
+
   render() {
     // this makes the code more readable
     const { first_name, last_name, email, bio, profile_picture } = this.props.currentUser;
@@ -15,18 +32,18 @@ export default class Sidebar extends React.Component {
           <h1 className='sidebar-name'>
              {first_name} {last_name}
           </h1>
-          <p className='sidebar-email'>
-            {email}
-          </p>
+          <div className='sidebar-email'>
+            <span>{email}</span>
+          </div>
         </div>
         <div className='sidebar-card'>
-          <h2 className='sidebar-title'>About Me</h2>
+          <h2 className='sidebar-title'>Your Bio</h2>
           <div className='sidebar-bio-content'>
             <span>'{bio}'</span>
           </div>
         </div>
         <div className='sidebar-card'>
-          <h2 className='sidebar-title'>Interests</h2>
+          <h2 className='sidebar-title'>Your Interests</h2>
           <ul className='sidebar-interests-list'>
             {current_interests.map((interest, i) =>
               <li key={i}>{interest}</li>
@@ -34,7 +51,7 @@ export default class Sidebar extends React.Component {
           </ul>
         </div>
         <div className='sidebar-notifications'>
-          <Notifications notifications={notifications} />
+          <Notifications notifications={notifications} deleteNotification={this.deleteNotification} />
         </div>
       </div>
     );
